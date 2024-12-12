4e2596d3f8c00171f08596d9cd356e4cea01f030
Igor Braeckevelt <igor.braeckevelt@gmail.com>
2024-12-12 14:34:32 +0100

    Properties pins on map + popup view

--- src/components/mapview/MapView.tsx
+++ src/components/mapview/MapView.tsx
@@ -16,22 +16,59 @@ const MapView = ({ properties }: MapViewProps) => {
     useEffect(() => {
         if (!mapContainer.current) return;
 
-        // Initialize map
         mapboxgl.accessToken = 'pk.eyJ1IjoiaWdvcjUwMiIsImEiOiJjbTQ1bmdnN3gwdmRvMmxxeDgwOG12M2gxIn0.UxOguMQKWi0366_3MF45mw';
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/outdoors-v12', // Outdoor style fits camping theme
-            center: [4.3517, 50.8503], // Brussels center coordinates
+            style: 'mapbox://styles/mapbox/outdoors-v12',
+            center: [4.3517, 50.8503],
             zoom: 8
         });
 
-        // Add controls
         map.current.addControl(new mapboxgl.NavigationControl());
+
+        map.current.on('load', () => {
+            properties.forEach((property) => {
+                if (property.address.latitude && property.address.longitude) {
+                    const testImage = "https://media.istockphoto.com/id/1377841262/photo/the-beautiful-scenery-of-a-tent-in-a-pine-tree-forest-at-pang-oung-mae-hong-son-province.jpg?s=612x612&w=0&k=20&c=1JvDx-16zTIeytdcC-Fa27nVJ_8SveP-omNKKlUJ-lQ=";
+
+                    const popup = new mapboxgl.Popup({
+                        offset: 25,
+                        maxWidth: '300px'
+                    }).setHTML(
+                        `<div class="map-popup">
+                            <h5>${property.name}</h5>
+                            <div class="image-gallery">
+                                <img src="${testImage}" alt="${property.name}" />
+                                <img src="${testImage}" alt="${property.name}" />
+                                <img src="${testImage}" alt="${property.name}" />
+                            </div>
+                            <p class="description">${property.description}</p>
+                            <p class="price">€${property.pricePerNight}/night</p>
+                        </div>`
+                    );
+
+                    new mapboxgl.Marker({
+                        color: "#2C5530",
+                    })
+                        .setLngLat([property.address.longitude, property.address.latitude])
+                        .setPopup(popup)
+                        .addTo(map.current!);
+                }
+            });
+
+            const bounds = new mapboxgl.LngLatBounds();
+            properties.forEach(property => {
+                if (property.address.latitude && property.address.longitude) {
+                    bounds.extend([property.address.longitude, property.address.latitude]);
+                }
+            });
+            map.current!.fitBounds(bounds, { padding: 50 });
+        });
 
         return () => {
             map.current?.remove();
         };
-    }, []);
+    }, [properties]);
 
     return (
         <div className="map-container" ref={mapContainer} />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,45 @@
 // src/pages/HomePage/index.tsx
 import {Container} from 'react-bootstrap';
-import MapView from '../components/mapview/MapView';
-import {properties} from '../util/TestData'; // Import test data
+import DatePicker from 'react-datepicker';
+import MapView from '../components/mapview/MapView.tsx';
+import {properties} from '../util/TestData.ts';
+import {useEffect, useState} from 'react';
+import {Calendar} from 'react-bootstrap-icons'; // Add this import
+import "react-datepicker/dist/react-datepicker.css";
 import './HomePage.scss';
 
 const HomePage = () => {
+    const [startDate, setStartDate] = useState<Date | undefined>(undefined);
+    const [endDate, setEndDate] = useState<Date | undefined>(undefined);
+
+
+    useEffect(() => {
+        console.log(startDate, endDate);
+    });
+
     return (
         <div className="home-page">
-            <Container className="py-4 mapview">
-                <h1 className="mb-4">Find your perfect camping spot</h1>
+            <Container fluid="xxl" className="py-4">
+                <div className="date-picker-container mb-4 mx-auto">
+                    <div className="input-wrapper">
+                        <Calendar className="calendar-icon"/>
+                        <DatePicker
+                            selectsRange={true}
+                            startDate={startDate}
+                            endDate={endDate}
+                            onChange={(dates: [Date | null, Date | null]) => {
+                                const [start, end] = dates;
+                                setStartDate(start ?? undefined);
+                                setEndDate(end ?? undefined);
+                            }}
+                            dateFormat="dd/MM/yyyy"
+                            minDate={new Date()}
+                            placeholderText="Select check-in and check-out dates"
+                            className="form-control"
+                            monthsShown={2}
+                        />
+                    </div>
+                </div>
                 <MapView properties={properties}/>
             </Container>
         </div>
